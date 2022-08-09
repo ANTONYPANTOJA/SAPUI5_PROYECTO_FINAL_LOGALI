c3b7fd774492577f657c3175cc9869ffbae35865
ANTONYPANTOJA <anthonypantojaari@gmail.com>
2022-08-09 21:18:15 +0000

    Proyecto_Final_Fiori_v2_0

--- RRHH_EMPLOYEES/rrhh_employees/webapp/controller/CreateEmployee.controller.js
+++ RRHH_EMPLOYEES/rrhh_employees/webapp/controller/CreateEmployee.controller.js
@@ -34,46 +34,133 @@ sap.ui.define([
         },
         Step2: function (oEvent) {
 
+            //Obtener los ID's de los wizards
+
+            var id_typeEmployeeStep = this.byId("typeEmployeeStep"); // Form 1
+            var id_dataEmployeeStep = this.byId("dataEmployeeStep"); // form 2
+
             //Validar el tipo de empleado seleccionado
             var obutton_type_employee = oEvent.getSource();
             var type_employee = obutton_type_employee.data("typeEmployee");
 
             var oModel = {
-                            salary:"",
-                            type:0,
-                            typeEmployee:""
+                salary: "",
+                type: 0,
+                _typeEmployee: ""
             };
 
             switch (type_employee) {
                 case "interno":
-                    oModel.typeEmployee = type_employee;
+                    oModel._typeEmployee = type_employee;
                     oModel.type = 0;
                     oModel.salary = "24000";
                     break;
                 case "autonomo":
-                    oModel.typeEmployee = type_employee;
+                    oModel._typeEmployee = type_employee;
                     oModel.type = 1;
                     oModel.salary = "24000";
                     break;
-                
+
                 case "gerente":
-                    oModel.typeEmployee = type_employee;
-                    oModel.type         = 2;
-                    oModel.salary       = "70000";
-                    break; 
+                    oModel._typeEmployee = type_employee;
+                    oModel.type = 2;
+                    oModel.salary = "70000";
+                    break;
 
                 default:
                     break;
             }
 
-        //Set de los datos
-        this._modelEmployee.setData({ typeEmployee  : oModel.typeEmployee,
-                                      type          : oModel.type,
-                                      salary        : oModel.salary
-                                    });
+            //Set de los datos al modelo crado de forma interna
+            this._modelEmployee.setData({
+                _typeEmployee: oModel._typeEmployee,
+                type: oModel.type,
+                salary: oModel.salary
+            });
 
             //Validar el siguiente Paso NEXT STEP
-            this._wizard.nextStep();
+            if (this._wizard.getCurrentStep() === id_typeEmployeeStep.getId()) {
+                this._wizard.nextStep();
+            } else {
+                this._wizard.goToStep(id_dataEmployeeStep);
+            }
+
+        },
+        validateDNI: function (oEvent) {
+
+            //Validar el DNI para los tipos INTERNO Y GERENTE
+
+            if (this._modelEmployee.getProperty("_typeEmployee") !== "autonomo") {
+                var dni = oEvent.getParameter("value");
+                var number;
+                var letter;
+                var letterList;
+                var regularExp = /^\d{8}[a-zA-Z]$/;
+                //Se comprueba que el formato es válido
+                if (regularExp.test(dni) === true) {
+                    //Número
+                    number = dni.substr(0, dni.length - 1);
+                    //Letra
+                    letter = dni.substr(dni.length - 1, 1);
+                    number = number % 23;
+                    letterList = "TRWAGMYFPDXBNJZSQVHLCKET";
+                    letterList = letterList.substring(number, number + 1);
+                    if (letterList !== letter.toUpperCase()) {
+                        this._modelEmployee.setProperty("/_DniState", "Error");
+                    } else {
+                        this._modelEmployee.setProperty("/_DniState", "None");
+                        this.onEmployeeValidation();//Validar los otros campos
+                    }
+                } else {
+                    this._modelEmployee.setProperty("/_DniState", "Error");
+                }
+            }
+        },
+        onEmployeeValidation: function (oEvent) {
+            //Validar que los campos este rellenos
+            var oModel_employee = this._modelEmployee.getData();
+            var onValidation = true;
+
+            //Nombre
+            if (!oModel_employee.FirstName) {
+                oModel_employee._FirstNameState = "Error";
+                onValidation = false;
+                return;
+            } else {
+                oModel_employee._FirstNameState = "None";
+            }
+            //Apellido
+            if (!oModel_employee.LastName) {
+                oModel_employee._LastNameState = "Error";
+                onValidation = false;
+                return;
+            } else {
+                oModel_employee._LastNameState = "None";
+            }
+            //DNI
+            if (!oModel_employee.Dni) {
+                oModel_employee._DniState = "Error";
+                onValidation = false;
+                return;
+            } else {
+                oModel_employee._DniState = "None";
+            }
+
+            //Fecha
+            if (!oModel_employee.CreationDate) {
+                oModel_employee._CreationDateState = "Error";
+                onValidation = false;
+                return;
+            }else{
+                oModel_employee._CreationDateState = "None";
+            }
+
+            //Pasar los datos
+            if (onValidation === true) {
+                this._wizard.validateStep(this.byId("dataEmployeeStep"));
+            }
+
+
         },
         onCancel: function () {
 
