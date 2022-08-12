1ec3aa6ad63e697908cf0119f6498eada78cb007
ANTONYPANTOJA <anthonypantojaari@gmail.com>
2022-08-12 23:00:27 +0000

    Proyecto_Final_Fiori_v4_0

--- RRHH_EMPLOYEES/rrhh_employees/webapp/controller/CreateEmployee.controller.js
+++ RRHH_EMPLOYEES/rrhh_employees/webapp/controller/CreateEmployee.controller.js
@@ -11,7 +11,7 @@ sap.ui.define([
          * @param {typeof sap.m.UploadCollectionParameter} UploadCollectionParameter
          */
 
-], function (Controller,MessageBox,MessageToast,UploadCollectionParameter) {
+], function (Controller, MessageBox, MessageToast, UploadCollectionParameter) {
     'use strict';
 
     return Controller.extend("logaligroup.rrhhemployees.controller.CreateEmployee", {
@@ -115,7 +115,8 @@ sap.ui.define([
                         this._modelEmployee.setProperty("/_DniState", "Error");
                     } else {
                         this._modelEmployee.setProperty("/_DniState", "None");
-                        this.onEmployeeValidation();//Validar los otros campos
+                        //this.onEmployeeValidation();//Validar los otros campos
+                        //return true;
                     }
                 } else {
                     this._modelEmployee.setProperty("/_DniState", "Error");
@@ -149,7 +150,7 @@ sap.ui.define([
                 onValidation = false;
                 return;
             } else {
-                oModel_employee._DniState = "None";
+                 oModel_employee._DniState = "None";
             }
 
             //Fecha
@@ -157,7 +158,7 @@ sap.ui.define([
                 oModel_employee._CreationDateState = "Error";
                 onValidation = false;
                 return;
-            }else{
+            } else {
                 oModel_employee._CreationDateState = "None";
             }
 
@@ -168,125 +169,153 @@ sap.ui.define([
 
 
         },
-        wizardCompletedHandler:function(oEvent){
+        wizardCompletedHandler: function (oEvent) {
 
-            	//Se navega a la página review
-				var wizardNavContainer = this.byId("wizardNavContainer");
-				wizardNavContainer.to(this.byId("ReviewPage"));
+            //Se navega a la página review
+            var wizardNavContainer = this.byId("wizardNavContainer");
+            wizardNavContainer.to(this.byId("ReviewPage"));
 
-                //Obtener los archivos cargados
-                var uploadCollection = this.byId("UploadCollection");
-				var files = uploadCollection.getItems();
-				var numFiles = uploadCollection.getItems().length;
-				this._modelEmployee.setProperty("/_numFiles",numFiles);
+            //Obtener los archivos cargados
+            var uploadCollection = this.byId("UploadCollection");
+            var files = uploadCollection.getItems();
+            var numFiles = uploadCollection.getItems().length;
+            this._modelEmployee.setProperty("/_numFiles", numFiles);
 
-                if (numFiles > 0) {
-					var arrayFiles = [];
-					for(var i in files){
-						arrayFiles.push({DocName:files[i].getFileName(),MimeType:files[i].getMimeType()});	
-					}
-					this._modelEmployee.setProperty("/_files",arrayFiles);
-				}else{
-					this._modelEmployee.setProperty("/_files",[]);
-				}
+            if (numFiles > 0) {
+                var arrayFiles = [];
+                for (var i in files) {
+                    arrayFiles.push({ DocName: files[i].getFileName(), MimeType: files[i].getMimeType() });
+                }
+                this._modelEmployee.setProperty("/_files", arrayFiles);
+            } else {
+                this._modelEmployee.setProperty("/_files", []);
+            }
 
-                
 
-                //Binding the Elements
-                //var txtTypeEmployee = this.getById("txtTypeEmployee");
-                //txtTypeEmployee.bindProperty("value", "/_typeEmployee");
 
-                //this._modelEmployee.bind(this);
+            //Binding the Elements
+            //var txtTypeEmployee = this.getById("txtTypeEmployee");
+            //txtTypeEmployee.bindProperty("value", "/_typeEmployee");
+
+            //this._modelEmployee.bind(this);
 
         },
-        stepEdit: function(id_step){
+        stepEdit: function (id_step) {
             var wizardNavContainer = this.byId("wizardNavContainer");
             var fnAfterNavigate = function () {
-				this._wizard.goToStep(this.byId(id_step));
-				//Se quita la función para que no vuelva a ejecutar al volver a navegar
-				wizardNavContainer.detachAfterNavigate(fnAfterNavigate);
-			}.bind(this);
+                this._wizard.goToStep(this.byId(id_step));
+                //Se quita la función para que no vuelva a ejecutar al volver a navegar
+                wizardNavContainer.detachAfterNavigate(fnAfterNavigate);
+            }.bind(this);
 
-		    wizardNavContainer.attachAfterNavigate(fnAfterNavigate);
-		    wizardNavContainer.back();
+            wizardNavContainer.attachAfterNavigate(fnAfterNavigate);
+            wizardNavContainer.back();
         },
-        pasoOne: function(){
+        pasoOne: function () {
             this.stepEdit.bind(this)("typeEmployeeStep");
         },
-        pasoTwo: function(){
+        pasoTwo: function () {
             this.stepEdit.bind(this)("dataEmployeeStep");
         },
-        pasoThree: function(){
+        pasoThree: function () {
             this.stepEdit.bind(this)("dataInfoAdicional");
         },
         onCancel: function () {
 
-            MessageBox.confirm(this.oView.getModel("i18n").getResourceBundle().getText("preguntaCancelar"),{
-                onClose : function(oAction){
-                    if(oAction === "OK"){
+            MessageBox.confirm(this.oView.getModel("i18n").getResourceBundle().getText("preguntaCancelar"), {
+                onClose: function (oAction) {
+                    if (oAction === "OK") {
                         //Regresamos al menú principal
                         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                        oRouter.navTo("RouteApp",{},true);//Navegar al menú principal
+                        oRouter.navTo("RouteApp", {}, true);//Navegar al menú principal
                     }
                 }.bind(this)
             });
 
         },
-        onSaveEmployee: function(){
+        onSaveEmployee: function () {
 
             var dataJson = this.getView().getModel().getData();
 
             var inputDataBody = {
                 SapId: this.getOwnerComponent().SapId,
-                Type:           parseInt(dataJson.type).toString(),
-                FirstName:      dataJson.FirstName,
-                LastName:       dataJson.LastName,
-                Dni:            dataJson.Dni,
-                CreationDate:   dataJson.CreationDate,
-                Comments:       dataJson.comentario
+                Type: parseInt(dataJson.type).toString(),
+                FirstName: dataJson.FirstName,
+                LastName: dataJson.LastName,
+                Dni: dataJson.Dni,
+                CreationDate: dataJson.CreationDate,
+                Comments: dataJson.comentario
             };
 
             //Rellenar el Salary
             inputDataBody.UserToSalary = [{
-                                            Amount : parseFloat(dataJson.salary).toString(),
-                                            Comments : dataJson.comentario,
-                                            Waers : "EUR" }];
+                Amount: parseFloat(dataJson.salary).toString(),
+                Comments: dataJson.comentario,
+                Waers: "EUR"
+            }];
 
             this.getView().setBusy(true);
-            console.log(inputDataBody);
+            //console.log(inputDataBody);
 
             //Llamar a la función POST - CREATE del Odata
-            this.getView().getModel("odataModel").create("/Users",inputDataBody,{
+            this.getView().getModel("odataModel").create("/Users", inputDataBody, {
 
-                success: function(data){
-                    console.log("ok:" + data );
+                success: function (data) {
+                    //console.log("ok:" + data);
                     this.getView().setBusy(false);
                     this.idUser = data.EmployeeId;
                     MessageBox.information(this.oView.getModel("i18n").getResourceBundle().getText("empleadoNew") + ": " + this.idUser,
-                    
-                    {
-                       onClose: function() {
-                        
-                        //Volver al Menú
-                        var wizardNavContainer = this.byId("wizardNavContainer");
-						wizardNavContainer.back();
 
-                        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                        oRouter.navTo("RouteApp",{},true);
+                        {
+                            onClose: function () {
+
+                                //Volver al Menú
+                                var wizardNavContainer = this.byId("wizardNavContainer");
+                                wizardNavContainer.back();
+
+                                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                                oRouter.navTo("RouteApp", {}, true);
 
 
-                       }.bind(this) 
-                    });
+                            }.bind(this)
+                        });
+                        this.onStartUpload();// función "upload" del uploadCollection
 
                 }.bind(this),
-                error: function(e){
+                error: function (e) {
                     this.getView().setBusy(false);
-                    console.log("error:" + e );
+                    console.log("error:" + e);
                     MessageToast.show(this.oView.getModel("i18n").getResourceBundle().getText("odataSaveKO"));
                 }.bind(this)
             });
 
-            }
+        },
+        onChange: function (oEvent) {
+
+            var oUploadCollection = oEvent.getSource();
+            // Header Token
+            var oCustomerHeaderToken = new UploadCollectionParameter({
+                name: "x-csrf-token",
+                value: this.getView().getModel("odataModel").getSecurityToken()
+            });
+
+            oUploadCollection.addHeaderParameter(oCustomerHeaderToken);
+
+        },
+        onBeforeUploadStart: function(oEvent){
+
+            var oCustomerHeaderSlug = new UploadCollectionParameter({
+				name: "slug",
+				value: this.getOwnerComponent().SapId+";"+this.idUser+";"+oEvent.getParameter("fileName")
+			});
+			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
+            console.log(oCustomerHeaderSlug);
+	  
+        },
+        onStartUpload: function(){
+	    var oUploadCollection = this.byId("UploadCollection");
+	        oUploadCollection.upload();
+        }
 
     });
 
